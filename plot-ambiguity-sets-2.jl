584e4a7566e0309c3efdd3bfb6f9b714db339049
dom UoA <dkee331@aucklanduni.ac.nz>
2025-09-22 22:14:18 +1200

    update plot

--- plot-ambiguity-sets-2.jl
+++ plot-ambiguity-sets-2.jl
@@ -26,10 +26,11 @@ default(framestyle = :box,
         legendfont = Plots.font(fontfamily, pointsize = 11),
         tickfont = Plots.font(fontfamily, pointsize = 10))
 
-plt = plot(xlims=(-3,3),
-            ylims=(0,2),
+plt = plot(xlims=(-0,5),
+            ylims=(0,3),
             xlabel="Mean", 
             ylabel="Standard deviation",
+            #legend=:horizontal,
             topmargin = 0pt, 
             rightmargin = 0pt,
             bottommargin = 6pt, 
@@ -51,10 +52,11 @@ function nonnegative_ellipse_coords(ellipse_parameters)
 
 end
 
-    intersection_ellipse_1 = [1.75,1.75,-1,0] # Width, height, x, y. 
-    intersection_ellipse_2 = [1.5,1.5,0,0]
-    intersection_ellipse_3 = [1.25,1.25,1,0]
-    weighted_ellipse = [1,1,0.85,0.25]
+    intersection_ellipse_1 = [2.75,2.75,1,0] # Width, height, x, y. 
+    intersection_ellipse_2 = [2.5,2.6,2,0]
+    intersection_ellipse_3 = [2.25,2.4,3,0]
+    intersection_ellipse_4 = [2,2.25,4,0]
+    weighted_ellipse = [0.95,0.95,2.95,0.95]
 
     linewidth = 1
     alpha = 1
@@ -81,6 +83,16 @@ end
             fill = (0, fillalpha, palette(:tab10)[1]))
 
     x_coords, y_coords = nonnegative_ellipse_coords(intersection_ellipse_3)
+    plot!(x_coords,
+            y_coords,
+            color = palette(:tab10)[1],
+            linewidth = 0,
+            linestyle = :solid,
+            alpha = 0,
+            label = nothing,
+            fill = (0, fillalpha, palette(:tab10)[1]))
+
+    x_coords, y_coords = nonnegative_ellipse_coords(intersection_ellipse_4)
     plot!(x_coords,
             y_coords,
             color = palette(:tab10)[1],
@@ -128,7 +140,7 @@ function nonnegative_intersected_ellipse_coords(ellipse_1_parameters, ellipse_2_
 
 end
 
-    x_coords, y_coords = nonnegative_intersected_ellipse_coords(intersection_ellipse_1, intersection_ellipse_3)
+    x_coords, y_coords = nonnegative_intersected_ellipse_coords(intersection_ellipse_1, intersection_ellipse_4)
     plot!(x_coords,
             y_coords,
             color = palette(:tab10)[1],
@@ -145,7 +157,7 @@ end
             linestyle = :solid,
             alpha = alpha,
             label = "Intersection",
-            fill = (0, 2*fillalpha, palette(:tab10)[1]))
+            fill = (0, 3*fillalpha, palette(:tab10)[1]))
 
     x_coords, y_coords = nonnegative_ellipse_coords(weighted_ellipse)
     plot!(x_coords,
@@ -157,39 +169,41 @@ end
             label = "Weighted",
             fill = (0, 2*fillalpha, palette(:tab10)[2]))
 
-    scatter!([-1,0,1], [0,0,0], 
+    samples = [1,2,3,4]
+    scatter!(samples, zeros(length(samples)), 
                 markersize = 6.0,
                 markershape = :utriangle,
                 markercolor = :black,
                 markerstrokecolor = :black,
                 markerstrokewidth = 0,
-                alpha = 1, 
-                labels = nothing,)
-    annotate!(-1, 0, text(" \$\\xi_1\$", :black, :bottom, 12))
-    annotate!(0, 0, text(" \$\\xi_2\$", :black, :bottom, 12))
-    annotate!(1, 0, text(" \$\\xi_3\$", :black, :bottom, 12))
+                alpha = 1,
+                labels = nothing)
+
+    for i in eachindex(samples)
+        annotate!(samples[i], 0, text(" \$\\xi_$i\$", :black, :bottom, 12))
+    end
 
     display(plt)
     savefig(plt, "figures/ambiguity-sets.pdf")
 
 
 
-if true
+if false
 
-    Random.seed!(42)
+    p = 2
 
     env = Gurobi.Env()
     GRBsetintparam(env, "OutputFlag", 0)
     Linear_Optimizer = optimizer_with_attributes(() -> Gurobi.Optimizer(env))
 
-    function in_W2_ball(P, ε, Q)
+    function in_ball(P, ε, Q)
 
         # P, Q := [points, weights] .
 
         m = length(P[1])
         n = length(Q[1])
 
-        d = [abs(ξ - ζ)^2 for ξ in P[1], ζ in Q[1]]
+        d = [abs(ξ - ζ)^p for ξ in P[1], ζ in Q[1]]
 
         model = Model(Linear_Optimizer)
 
@@ -202,13 +216,13 @@ if true
 
         optimize!(model)
 
-        try; return ifelse((objective_value(model)^(1/2)) <= ε, 1, 0); catch; return 0; end
+        @assert is_solved_and_feasible(model)
+
+        try; return ifelse((objective_value(model)^(1/p)) <= ε, 1, 0); catch; return 0; end
 
     end
 
     include("weights.jl")
-
-    normalise(x) = x/sum(x)
 
     function mean_and_std(Q)
 
@@ -216,37 +230,41 @@ if true
 
     end
 
-    support = [-3,3]
-    height = 2
-    samples = [-1,0,1]
+    Ξ = [0,5]
+    max_σ = 3
+    samples = [1,2,3,4]
 
-    number_of_points = 30
-    number_of_distributions = 1000 # 30000
+    number_of_points = 100
+    number_of_distributions = 20000 # 30000
 
-    markersize = 3
+    markersize = 4
 
-    ε = 1
+    weighted_ε = 1
+    weighted_ρ = 1/10
 
-    ϱ = 1/4
+    scale_radii = 2
+    intersection_ε = scale_radii*weighted_ε
+    intersection_ρ = scale_radii*weighted_ρ
 
-    P = [samples, W2_concentration_weights(3, ϱ/ε)]
+    P = [samples, Wp_concentration_weights(p, length(samples), weighted_ρ/weighted_ε)]
 
     display(P)
 
     Qs = [[zeros(number_of_points), zeros(number_of_points)] for _ in 1:number_of_distributions]
     Threads.@threads for i in ProgressBar(eachindex(Qs))
         local n = number_of_points
-        local μ = rand(Uniform(support[1],support[end]))
-        local σ = rand(Uniform(0,height))
-        local points = rand(Normal(μ, σ), n)
+        local μ = rand(Uniform(Ξ[1],Ξ[end]))
+        local σ = rand(Uniform(0,max_σ))
+        #local points = rand(Normal(μ, σ), n)
+        local points = rand(Uniform(μ-sqrt(3)*σ, μ+sqrt(3)*σ), n)
 
         Qs[i] = [points, 1/n*ones(n)]
 
     end
 
-    plot_intersection_Qs = [zeros(3) for _ in eachindex(Qs)]
+    plot_intersection_Qs = [zeros(length(samples)) for _ in eachindex(Qs)]
     Threads.@threads for i in ProgressBar(eachindex(Qs))
-        plot_intersection_Qs[i] = [in_W2_ball([[P[1][j]],[1.0]],ε+(3-j+1)*ϱ,Qs[i]) for j in 1:3]
+        plot_intersection_Qs[i] = [in_ball([[P[1][j]],[1.0]],intersection_ε+(length(samples)-j+1)*intersection_ρ,Qs[i]) for j in 1:length(samples)]
 
     end
 
@@ -275,8 +293,8 @@ if true
             tickfont = secondary_font,
             legendfont = legend_font)
 
-    plt = plot(xlims=(support[1],support[end]), 
-                ylims=(0,height), 
+    plt = plot(xlims=(Ξ[1],Ξ[end]), 
+                ylims=(0,max_σ), 
                 xlabel="Mean", 
                 ylabel="Standard deviation",           
                 topmargin = 0pt, 
@@ -284,48 +302,48 @@ if true
                 bottommargin = 6pt, 
                 leftmargin = 6pt)
 
-    tab10_primary_colour = [227,119,194]/255
+    color_increment = 0.05
+    pink = min.([227,119,194]/255 .+ length(samples)*color_increment, 1)
 
-    for i in ProgressBar(eachindex(Qs))
+    for i in eachindex(samples)
+        for j in ProgressBar(eachindex(Qs))
 
-        if sum(plot_intersection_Qs[i]) == 1
-            mean, std = mean_and_std(Qs[i])
-            scatter!([mean], [std], color=RGB(min(tab10_primary_colour[1]+0.1,1.0),tab10_primary_colour[2]+0.1,tab10_primary_colour[3]+0.1), markersize=markersize, markerstrokewidth=0.0, alpha=1, labels=nothing,)
-
+            if sum(plot_intersection_Qs[j]) == i
+                mean, std = mean_and_std(Qs[j])
+                intersection_color = max.(pink .- i*color_increment,0)
+                scatter!([mean], [std], 
+                            color = RGB(intersection_color[1],intersection_color[2],intersection_color[3]), 
+                            markersize = markersize,
+                            markerstrokewidth = 0.0,
+                            alpha = 1,
+                            labels = nothing)
+            end
         end
     end
 
-    for i in ProgressBar(eachindex(Qs))
-        if sum(plot_intersection_Qs[i]) == 2
-            mean, std = mean_and_std(Qs[i])
-            scatter!([mean], [std], color=RGB(tab10_primary_colour[1],tab10_primary_colour[2],tab10_primary_colour[3]), markersize=markersize, markerstrokewidth=0.0, alpha=1, labels=nothing,)
+    # intersection_color = pink .- length(samples)*color_increment
+    scatter!([-1], [-1], 
+                color = RGB(intersection_color[1],intersection_color[2],intersection_color[3]),
+                markersize = markersize, 
+                markerstrokewidth = 0.0, 
+                alpha = 1, 
+                label = "Intersection")
 
-        end
-    end
-
-    for i in ProgressBar(eachindex(Qs))
-        if sum(plot_intersection_Qs[i]) == 3
-            mean, std = mean_and_std(Qs[i])
-            scatter!([mean], [std], color=RGB(tab10_primary_colour[1]-0.1,tab10_primary_colour[2]-0.1,tab10_primary_colour[3]-0.1), markersize=markersize, markerstrokewidth=0.0, alpha=1, labels=nothing,)
-
-        end
-    end
-
-    scatter!([-1], [-1], color=RGB(tab10_primary_colour[1]-0.1,tab10_primary_colour[2]-0.1,tab10_primary_colour[3]-0.1), markersize=markersize, markerstrokewidth=0.0, alpha=1, label="Intersection",)
-
-    scatter!(samples, [0,0,0], 
+    scatter!(samples, zeros(length(samples)), 
                 markersize = 6.0,
                 markershape = :utriangle,
-                markercolor = :black,#palette(:tab10)[8],
+                markercolor = :black,
                 markerstrokecolor = :black,
-                markerstrokewidth = 0,#,0.5,
-                alpha=1, labels=nothing,)
-    annotate!(samples[1], 0, text(" \$\\xi_1\$", :black, :bottom, 12))
-    annotate!(samples[2], 0, text(" \$\\xi_2\$", :black, :bottom, 12))
-    annotate!(samples[3], 0, text(" \$\\xi_3\$", :black, :bottom, 12))
+                markerstrokewidth = 0,
+                alpha = 1,
+                labels = nothing)
+
+    for i in eachindex(samples)
+        annotate!(samples[i], 0, text(" \$\\xi_$i\$", :black, :bottom, 12))
+    end
 
     display(plt)
-    #savefig(plt, "figures/ambiguity-sets-data-1.pdf")
+    savefig(plt, "figures/ambiguity-sets-data-1.pdf")
 
     default() # Reset plot defaults.
 
@@ -352,8 +370,8 @@ if true
             tickfont = secondary_font,
             legendfont = legend_font)
 
-    plt = plot(xlims=(support[1],support[end]), 
-                ylims=(0,height), 
+    plt = plot(xlims=(Ξ[1],Ξ[end]), 
+                ylims=(0,max_σ), 
                 xlabel="Mean", 
                 ylabel="Standard deviation",
                 topmargin = 0pt, 
@@ -361,86 +379,47 @@ if true
                 bottommargin = 6pt, 
                 leftmargin = 6pt)
 
-
-    tab10_primary_colour = [227,119,194]/255
-
-    for i in ProgressBar(eachindex(Qs))
-
-        if sum(plot_intersection_Qs[i]) == 1
-            mean, std = mean_and_std(Qs[i])
-            scatter!([mean], [std], color=RGB(min(tab10_primary_colour[1]+0.1,1.0),tab10_primary_colour[2]+0.1,tab10_primary_colour[3]+0.1), markersize=markersize, markerstrokewidth=0.0, alpha=1, labels=nothing,)
-
-        end
-    end
-
-    for i in ProgressBar(eachindex(Qs))
-        if sum(plot_intersection_Qs[i]) == 2
-            mean, std = mean_and_std(Qs[i])
-            scatter!([mean], [std], color=RGB(tab10_primary_colour[1],tab10_primary_colour[2],tab10_primary_colour[3]), markersize=markersize, markerstrokewidth=0.0, alpha=1, labels=nothing,)
-
-        end
-    end
-
-    for i in ProgressBar(eachindex(Qs))
-        if sum(plot_intersection_Qs[i]) == 3
-            mean, std = mean_and_std(Qs[i])
-            scatter!([mean], [std], 
-                        color = RGB(tab10_primary_colour[1]-0.1,tab10_primary_colour[2]-0.1,tab10_primary_colour[3]-0.1), 
-                        markersize = markersize, 
-                        markerstrokewidth = 0.0, 
-                        alpha = 1, 
-                        labels = nothing,)
-
-        end
-    end
-
-    scatter!([-1], [-1], 
-                color = RGB(tab10_primary_colour[1]-0.1,tab10_primary_colour[2]-0.1,tab10_primary_colour[3]-0.1), 
-                markersize = markersize, 
-                markerstrokewidth = 0.0, 
-                alpha = 1,
-                label = "Intersection")
-
     plot_ball_Qs = zeros(length(Qs))
     Threads.@threads for i in ProgressBar(eachindex(Qs))
-        plot_ball_Qs[i] = in_W2_ball(P,ε,Qs[i])
+        plot_ball_Qs[i] = in_ball(P,weighted_ε,Qs[i])
 
     end
 
-    tab10_primary_colour = [188, 189, 34]/255
+    olive = [188, 189, 34]/255
 
     for i in ProgressBar(eachindex(Qs))
         if plot_ball_Qs[i] == 1
             mean, std = mean_and_std(Qs[i])
             scatter!([mean], [std], 
-                        color = RGB(tab10_primary_colour[1],tab10_primary_colour[2],tab10_primary_colour[3]), 
+                        color = RGB(olive[1],olive[2],olive[3]), 
                         markersize = markersize, 
                         markerstrokewidth = 0.0, 
-                        labels = nothing,)
+                        labels = nothing)
 
         end
     end
 
     scatter!([-1], [-1], 
-                color = RGB(tab10_primary_colour[1],tab10_primary_colour[2],tab10_primary_colour[3]), 
+                color = RGB(olive[1],olive[2],olive[3]), 
                 markersize = markersize, 
                 markerstrokewidth = 0.0, 
                 alpha = 1, 
                 label = "Weighted")
 
-    scatter!(samples, [0,0,0], 
+    scatter!(samples, zeros(length(samples)), 
                 markersize = 6.0,
                 markershape = :utriangle,
                 markercolor = :black,
                 markerstrokecolor = :black,
                 markerstrokewidth = 0,
-                alpha = 1, 
+                alpha = 1,
                 labels = nothing)
-    annotate!(samples[1], 0, text(" \$\\xi_1\$", :black, :bottom, 12))
-    annotate!(samples[2], 0, text(" \$\\xi_2\$", :black, :bottom, 12))
-    annotate!(samples[3], 0, text(" \$\\xi_3\$", :black, :bottom, 12))
+
+    for i in eachindex(samples)
+        annotate!(samples[i], 0, text(" \$\\xi_$i\$", :black, :bottom, 12))
+    end
 
     display(plt)
-    #savefig(plt, "figures/ambiguity-sets-data-2.pdf")
+    savefig(plt, "figures/ambiguity-sets-data-2.pdf")
 
 end
