2e2bd92b8a73e40acc90eca89ee6e23034a07bb9
Dominickk <dominickeehan@gmail.com>
2025-10-14 11:41:07 +1300

    fix weights plot

--- plot-weights.jl
+++ plot-weights.jl
@@ -4,7 +4,7 @@ include("weights.jl")
 
 default() # Reset plot defaults.
 
-gr(size = (275+6,183+6).*sqrt(3))
+gr(size = (275+6+6,183+6).*sqrt(3))
 fontfamily = "Computer Modern"
 default(framestyle = :box,
         grid = true,
@@ -24,10 +24,10 @@ default(framestyle = :box,
 
 plt = plot(
         xlabel = "Time index, \$t\$", 
-        ylabel = "Observation weight, \$w_t\$",
+        ylabel = "Weight, \$w_t\$",
         xticks = ([0, 25, 50, 75, 100]),
         topmargin = 0pt, 
-        rightmargin = 0pt,
+        rightmargin = 6pt,
         bottommargin = 6pt, 
         leftmargin = 6pt)
 
@@ -37,28 +37,37 @@ T = 100
 P = [1,2,3,4,5]
 
 linewidth = 1
-colors = cgrad([palette(:tab10)[2], palette(:tab10)[1]], P[end])
+#colors = cgrad([palette(:tab10)[2], palette(:tab10)[1]], P[end])
+#colors = cgrad([:black, palette(:tab10)[8]], P[end])
+colors = cgrad([:black, :black], P[end])
 linestyles = [:solid, :dash, :dashdot, :dashdotdot, :dot]
-alpha = 0.05
+linewidths = LinRange(1,1.2,P[end])
+alpha = 0.025
 
 for p in P
         plot!(1:T, 
                 Wp_weights(p, T, ρ/(p*ε)),
                 label = "\$p=$p\$",
                 color = colors[p],
-                linewidth = linewidth,
+                linewidth = linewidths[p],
                 linestyle = linestyles[p],
                 alpha = 1,
                 fill = (0, alpha, colors[p]))
 
 end
 
-xlims!((-2,102))
+#xlims!((-2,102))
+#yl = ylims(plt)
+#ylims!((-0.0007,0.025+0.0007))#yl[2]))
+
+xlims!((-0,100))
 yl = ylims(plt)
 ylims!((0,yl[2]))
+
 display(plt)
 
 savefig(plt, "figures/weights-for-p.pdf")
+savefig(plt, "figures/weights-for-p.svg")
 
 
 
